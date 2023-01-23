dbd7f7f7fd04eeb8d3bb52f42a39aa6e947c7f4f
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-23 00:52:42 -0500

    fixes

--- src/main/java/com/reesedevelopment/greatneckzmanim/front/controllers/ZmanimController.java
+++ src/main/java/com/reesedevelopment/greatneckzmanim/front/controllers/ZmanimController.java
@@ -206,6 +206,7 @@ public class ZmanimController {
             }*/
         }
 // KolhaMinyanim insertion
+List<Minyan> allMinyan = minyanDAO.getAll();
 List<KolhaMinyanim> kolhaMinyanims = new ArrayList<>();
 
 for (Minyan minyan : enabledMinyanim) {
