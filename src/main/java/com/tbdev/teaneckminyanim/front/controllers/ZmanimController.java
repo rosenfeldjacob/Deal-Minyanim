a97737c973d17e5bd4e36fcd9f81b40d15936051
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-24 00:43:44 -0500

    time format

--- src/main/java/com/tbdev/teaneckminyanim/front/controllers/ZmanimController.java
+++ src/main/java/com/tbdev/teaneckminyanim/front/controllers/ZmanimController.java
@@ -114,7 +114,7 @@ public class ZmanimController {
         mv.getModel().put("onlyDate", onlyDateFormat.format(date));
 
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, MMMM DD, yyyy HH:mm a");
         Date datenow = new Date();
         String timenow = dateFormat.format(datenow);
         mv.getModel().put("timenow", timenow);
