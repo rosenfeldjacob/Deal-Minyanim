c6e7fc90302b41cd4220fb944e2ebea9fb099207
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-24 12:00:42 -0500

    testing advanced markers

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -3,13 +3,19 @@ function initMap() {
     geocoder.geocode({'address': address}, function(results, status) {
       if (status === 'OK') {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
+          zoom: 15,
+          mapId: `ab35d05ad627f5db`,
           center: results[0].geometry.location
         });
-        var marker = new google.maps.Marker({
-          position: results[0].geometry.location,
-          map: map,
-          title: marker
+        const priceTag = document.createElement("div");
+
+        priceTag.className = "price-tag";
+        priceTag.textContent = marker;
+        
+        const markerView = new google.maps.marker.AdvancedMarkerView({
+          map,
+          position: { lat: 40.888489, lng: -74.0123889 },
+          content: priceTag,
         });
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
