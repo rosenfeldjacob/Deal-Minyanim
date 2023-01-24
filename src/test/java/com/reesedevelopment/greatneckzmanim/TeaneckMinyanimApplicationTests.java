25f5dd90a816a93f988e1400ebabc5b8009049b7
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-24 00:10:59 -0500

    more renaming

---
+++ src/test/java/com/reesedevelopment/greatneckzmanim/TeaneckMinyanimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbdev.teaneckminyanim;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class TeaneckMinyanimApplicationTests {
+
+//    @Test
+    void contextLoads() {
+    }
+
+}
