47d72995168adc013876e123a08c5e94ec7861d6
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-24 14:56:23 -0500

    chat gpt combine two examples

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -2,32 +2,74 @@ function initMap() {
   var geocoder = new google.maps.Geocoder();
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === 'OK') {
-      var map = new google.maps.Map(document.getElementById('map'), {
+      const center = results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
-        mapId: `ab35d05ad627f5db`,
-        center: results[0].geometry.location
+        center,
+        mapId: "ab35d05ad627f5db",
       });
-      const priceTag = document.createElement("div");
 
-      priceTag.className = "price-tag";
-      priceTag.textContent = marker;
-
-      const markerView = new google.maps.marker.AdvancedMarkerView({
-        map,
-        position: results[0].geometry.location,
-        content: priceTag,
-      });
-      google.maps.event.addListener(markerView, 'mouseover', function() {
+      for (const property of properties) {
+        const advancedMarkerView = new google.maps.marker.AdvancedMarkerView({
+          map,
+          content: buildContent(property),
+          position: property.position,
+          title: property.description,
+        });
+        const element = advancedMarkerView.element;
+        
+        const priceTag = document.createElement("div");
+        priceTag.className = "price-tag";
+        priceTag.textContent = property.price;
+        advancedMarkerView.content.appendChild(priceTag);
+        
+        ["focus", "pointerenter"].forEach((event) => {
+          element.addEventListener(event, () => {
+            highlight(advancedMarkerView, property);
+          });
+        });
+        ["blur", "pointerleave"].forEach((event) => {
+          element.addEventListener(event, () => {
+            unhighlight(advancedMarkerView, property);
+          });
+        });
+        advancedMarkerView.addListener("click", (event) => {
+          unhighlight(advancedMarkerView, property);
+        });
+        google.maps.event.addListener(advancedMarkerView, 'mouseover', function() {
           var extendedInfo = document.createElement("div");
           extendedInfo.className = "extended-info";
-          extendedInfo.innerHTML = "The Next Minyan is " + minyantype + " at " + minyantime;
+          extendedInfo.innerHTML = "The Next Minyan: " + minyantype + " at " + minyantime;
           priceTag.appendChild(extendedInfo);
-      });
-      google.maps.event.addListener(markerView, 'mouseout', function() {
+        });
+        google.maps.event.addListener(advancedMarkerView, 'mouseout', function() {
           priceTag.removeChild(extendedInfo);
-      });
+        });
+      }
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
 }
+
+function highlight(markerView, property) {
+  markerView.content.classList.add("highlight");
+  markerView.element.style.zIndex = 1;
+}
+
+function unhighlight(markerView, property) {
+  markerView.content.classList.remove("highlight");
+  markerView.element.style.zIndex = "";
+}
+
+function buildContent(property) {
+  const content = document.createElement("div");
+
+  content.classList.add("property");
+  content.innerHTML = `
+    <div class="icon">
+        <i aria-hidden="true" class="fa fa-icon fa-${property.type}" title="${property.type}"></i>
+        <span class="fa-sr-only">${property.type}</span>
+    </div>
+    <div class="details">
+    `}
