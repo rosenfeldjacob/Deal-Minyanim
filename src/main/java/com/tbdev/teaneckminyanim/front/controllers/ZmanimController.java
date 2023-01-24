d0a689d4326113874bf96a43de01b44368e30b44
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-24 09:51:36 -0500

    timenow on orgs

--- src/main/java/com/tbdev/teaneckminyanim/front/controllers/ZmanimController.java
+++ src/main/java/com/tbdev/teaneckminyanim/front/controllers/ZmanimController.java
@@ -473,4 +473,15 @@ mv.getModel().put("uniqueKolhaMinyanims", uniqueKolhaMinyanims);
     public ModelAndView orgToday(@PathVariable String orgId) throws Exception {
         return org(orgId, new Date());
     }
+    public ModelAndView orgs(Date date) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("orgs");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, MMM DD, yyyy h:mm a");
+        Date datenow = new Date();
+        String timenow = dateFormat.format(datenow);
+        mv.getModel().put("timenow", timenow);
+    
+        return mv;
+    }
 }
