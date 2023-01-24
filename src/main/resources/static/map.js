2b008010d975fe0a8a105aaf9b8277ff30f6156b
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-24 14:14:29 -0500

    map revert

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -1,66 +1,24 @@
 function initMap() {
-    var property
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'address': address}, function(results, status) {
-      if (status === 'OK') {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          mapId: `ab35d05ad627f5db`,
-          center: results[0].geometry.location
-        });
-            const advancedMarkerView = new google.maps.marker.AdvancedMarkerView({
-              map,
-              position: results[0].geometry.location,
-              content: buildContent(property),
-              title: marker,
-            });
-            const element = advancedMarkerView.element;
-        
-            ["focus", "pointerenter"].forEach((event) => {
-              element.addEventListener(event, () => {
-                highlight(advancedMarkerView, property);
-              });
-            });
-            ["blur", "pointerleave"].forEach((event) => {
-              element.addEventListener(event, () => {
-                unhighlight(advancedMarkerView, property);
-              });
-            });
-            advancedMarkerView.addListener("click", (event) => {
-              unhighlight(advancedMarkerView, property);
-            });
-          }
-        });
-        
-        function highlight(markerView, property) {
-          markerView.content.classList.add("highlight");
-          markerView.element.style.zIndex = 1;
-        };
-        
-        function unhighlight(markerView, property) {
-          markerView.content.classList.remove("highlight");
-          markerView.element.style.zIndex = "";
-        };
-        
-        function buildContent(property) {
-          const content = document.createElement("div");
-        
-          content.classList.add("property");
-          content.innerHTML = `
-            <div class="icon">
-                <i aria-hidden="true" class="fa fa-icon fa-building" th:each="minyan: ${allminyanim}" title="$${org.getName()}"></i>
-                <span class="fa-sr-only">building</span>
-            </div>
-            <div class="details">
-                <div class="price">Next Minyan</div>
-                
-                <div class="address" th:each="minyan: ${allminyanim}">${minyan.getType().displayName()[0]}</div>
-                <div class="address" th:each="minyan: ${allminyanim}">at</div>
-                <div class="address" th:each="minyan: ${allminyanim}">${minyan.getFormattedStartTime().displayName()[0]}</div>
-                </div>
-            </div>
-            `;
-          return content;
-        }
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        mapId: `ab35d05ad627f5db`,
+        center: results[0].geometry.location
+      });
+      const priceTag = document.createElement("div");
+
+      priceTag.className = "price-tag";
+      priceTag.textContent = marker;
+
+      const markerView = new google.maps.marker.AdvancedMarkerView({
+        map,
+        position: results[0].geometry.location,
+        content: priceTag,
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
-window.initMap = initMap;
