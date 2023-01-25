10df654426fbab2e9fdb2931c28a24b9b22ac6cc
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-25 15:06:05 -0500

    fix scroll, add dom

--- src/main/resources/static/scroll.js
+++ src/main/resources/static/scroll.js
@@ -1,13 +1,15 @@
-var section = document.getElementById("minyanim");
-var listExample = document.getElementById("list-example");
-
-window.addEventListener("scroll", function() {
-  var rect = section.getBoundingClientRect();
-  var isInView = rect.top <= (window.innerHeight || document.documentElement.clientHeight) && rect.bottom >= 0;
-
-  if (isInView) {
-    listExample.classList.add("fixed-top");
-  } else {
-    listExample.classList.remove("fixed-top");
-  }
-});
+document.addEventListener("DOMContentLoaded", function() {
+    var section = document.getElementById("minyanim");
+    var listExample = document.getElementById("list-example");
+  
+    window.addEventListener("scroll", function() {
+      var rect = section.getBoundingClientRect();
+      var isInView = rect.top <= (window.innerHeight || document.documentElement.clientHeight) && rect.bottom >= 0;
+  
+      if (isInView) {
+        listExample.classList.add("fixed-top");
+      } else {
+        listExample.classList.remove("fixed-top");
+      }
+    });
+  });  
