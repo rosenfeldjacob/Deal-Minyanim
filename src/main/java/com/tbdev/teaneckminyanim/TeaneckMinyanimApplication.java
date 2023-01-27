ed88da756ed5de4d12e1f753f3b1d2dc40f97aec
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-26 20:41:02 -0500

    Revert "beanstock"
    This reverts commit 85d0bbcc5e66d07772a0e3b91a39c7438c23bf9a.

--- src/main/java/com/tbdev/teaneckminyanim/TeaneckMinyanimApplication.java
+++ src/main/java/com/tbdev/teaneckminyanim/TeaneckMinyanimApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -14,14 +12,10 @@ import java.sql.Time;
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class TeaneckMinyanimApplication extends SpringBootServletInitializer {
+public class TeaneckMinyanimApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TeaneckMinyanimApplication.class, args);
         TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
-    }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(TeaneckMinyanimApplication.class);
     }
 }
