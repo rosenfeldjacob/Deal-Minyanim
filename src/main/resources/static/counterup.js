34570071731db7c3f95f73133045fe8dc197506b
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-29 00:43:20 -0500

    fixes

--- src/main/resources/static/counterup.js
+++
@@ -1,9 +0,0 @@
-import counterUp from 'counterup2'
-
-const el = document.querySelector( '.counter' )
-
-// Start counting, do this on DOM ready or with Waypoints.
-counterUp( el, {
-    duration: 1000,
-    delay: 16,
-} )
