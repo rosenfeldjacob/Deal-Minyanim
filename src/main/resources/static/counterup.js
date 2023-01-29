61b2ca5fc85890b965f483f902c6d6f0a95bec1d
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-29 00:41:17 -0500

    counter up 2 and js cleanup

---
+++ src/main/resources/static/counterup.js
@@ -0,0 +1,9 @@
+import counterUp from 'counterup2'
+
+const el = document.querySelector( '.counter' )
+
+// Start counting, do this on DOM ready or with Waypoints.
+counterUp( el, {
+    duration: 1000,
+    delay: 16,
+} )
