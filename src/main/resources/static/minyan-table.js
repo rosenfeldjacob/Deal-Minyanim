b0665e31b76c617701ffa7c7c571f90b5052be8b
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-02-20 13:15:12 -0500

    update styling and 5 increment

--- src/main/resources/static/minyan-table.js
+++ src/main/resources/static/minyan-table.js
@@ -1,32 +1,30 @@
-var table = document.getElementById("minyan-table");
-var loadMoreButton = document.getElementById("load-more-button");
-var numRowsToShow = 10;
-var numRowsShowing = 0;
+let table = document.getElementById("minyan-table");
+let rows = table.rows;
+let rowNum = 10; // the number of rows to display initially
+let increment = 5; // the number of rows to add each time the load more button is clicked
 
-// Hide rows that exceed the number of rows to show
-for (var i = numRowsToShow; i < table.rows.length; i++) {
-  table.rows[i].style.display = "none";
+function loadMore() {
+  let numRows = rows.length;
+
+  // add 5 more rows if there are still rows left to display
+  if (rowNum + increment < numRows) {
+    for (let i = rowNum; i < rowNum + increment; i++) {
+      rows[i].style.display = "";
+    }
+    rowNum += increment;
+  } else {
+    // if there are no more rows left to display, hide the load more button
+    for (let i = rowNum; i < numRows; i++) {
+      rows[i].style.display = "";
+    }
+    document.getElementById("load-more-button").style.display = "none";
+  }
 }
 
-// Update the number of rows showing
-numRowsShowing = numRowsToShow;
+// hide all rows except for the first 10
+for (let i = rowNum; i < rows.length; i++) {
+  rows[i].style.display = "none";
+}
 
-// Add a click event listener to the load more button
-loadMoreButton.addEventListener("click", function() {
-  // Show the next set of rows
-  for (var i = numRowsShowing; i < numRowsShowing + numRowsToShow; i++) {
-    if (i < table.rows.length) {
-      table.rows[i].style.display = "";
-    }
-  }
-
-  // Update the number of rows showing
-  numRowsShowing += numRowsToShow;
-
-  // Hide the load more button if all rows are showing
-  if (numRowsShowing >= table.rows.length) {
-    loadMoreButton.style.display = "none";
-  }
-});
-
-
+// add event listener to the load more button
+document.getElementById("load-more-button").addEventListener("click", loadMore);
