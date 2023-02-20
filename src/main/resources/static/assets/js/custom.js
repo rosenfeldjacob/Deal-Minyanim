c6042af779257f4cfa545d38851b89dbd1258f2a
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-02-19 23:57:23 -0500

    update

--- src/main/resources/static/assets/js/custom.js
+++ src/main/resources/static/assets/js/custom.js
@@ -103,25 +103,26 @@ jQuery(function($) {
 
   /* ----- Counter Up ----- */
 
-  const counterUp = window.counterUp.default
+  const counterUp = window.counterUp2.default;
 
   const callback = entries => {
-    entries.forEach( entry => {
-      const el = entry.target
-      if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
-        counterUp( el, {
+    entries.forEach(entry => {
+      const el = entry.target;
+      if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+        counterUp(el, {
           duration: 2000,
           delay: 16,
-        } )
-        el.classList.add( 'is-visible' )
+        });
+        el.classList.add('is-visible');
       }
-    } )
-  }
+    });
+  };
   
-  const IO = new IntersectionObserver( callback, { threshold: 1 } )
+  const IO = new IntersectionObserver(callback, { threshold: 1 });
   
-  const el = document.querySelector( '.counter' )
-  IO.observe( el )
+  const counters = document.querySelectorAll('.counter');
+  counters.forEach(counter => IO.observe(counter));
+  
 
 /*----- Preloader ----- */
 
