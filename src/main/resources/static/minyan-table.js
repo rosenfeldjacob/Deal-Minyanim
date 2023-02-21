733a4387c0b8ef97806342538d89b8fd678ec6a9
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-02-21 14:52:24 -0500

    revert

--- src/main/resources/static/minyan-table.js
+++ src/main/resources/static/minyan-table.js
@@ -1,46 +1,36 @@
-let tables = document.querySelectorAll(".minyan-table");
-let rows = []; // initialize an empty array to store all rows
-let rowNums = []; // initialize an empty array to store the current row numbers
-let increments = [5, 5]; // the number of rows to add each time the load more button is clicked for each table
-let loadMoreButtons = document.querySelectorAll(".load-more-button");
+let table = document.getElementById("minyan-table");
+let rows = table.rows;
+let rowNum = 10; // the number of rows to display initially
+let increment = 5; // the number of rows to add each time the load more button is clicked
+let loadMoreButton = document.getElementById("load-more-button");
 
-// loop through all tables to get the rows and set up the load more button
-for (let i = 0; i < tables.length; i++) {
-  let table = tables[i];
-  rows[i] = table.rows;
-  rowNums[i] = 10; // the number of rows to display initially
+function loadMore() {
+  let numRows = rows.length;
 
-  // hide all rows except for the first 10
-  for (let j = rowNums[i]; j < rows[i].length; j++) {
-    rows[i][j].style.display = "none";
-  }
-
-  // hide load more button if there are 5 or fewer rows
-  if (rows[i].length <= 5) {
-    loadMoreButtons[i].style.display = "none";
+  // add 5 more rows if there are still rows left to display
+  if (rowNum + increment < numRows) {
+    for (let i = rowNum; i < rowNum + increment; i++) {
+      rows[i].style.display = "";
+    }
+    rowNum += increment;
   } else {
-    // add event listener to the load more button
-    loadMoreButtons[i].addEventListener("click", function () {
-      loadMore(table, i);
-    });
+    // if there are no more rows left to display, hide the load more button
+    for (let i = rowNum; i < numRows; i++) {
+      rows[i].style.display = "";
+    }
+    loadMoreButton.style.display = "none";
   }
 }
 
-function loadMore(table, i) {
-  let numRows = rows[i].length;
-  let increment = increments[i];
+// hide all rows except for the first 10
+for (let i = rowNum; i < rows.length; i++) {
+  rows[i].style.display = "none";
+}
 
-  // add 5 more rows if there are still rows left to display
-  if (rowNums[i] + increment < numRows) {
-    for (let j = rowNums[i]; j < rowNums[i] + increment; j++) {
-      rows[i][j].style.display = "";
-    }
-    rowNums[i] += increment;
-  } else {
-    // if there are no more rows left to display, hide the load more button
-    for (let j = rowNums[i]; j < numRows; j++) {
-      rows[i][j].style.display = "";
-    }
-    loadMoreButtons[i].style.display = "none";
-  }
+// hide load more button if there are 5 or fewer rows
+if (rows.length <= 5) {
+  loadMoreButton.style.display = "none";
+} else {
+  // add event listener to the load more button
+  loadMoreButton.addEventListener("click", loadMore);
 }
