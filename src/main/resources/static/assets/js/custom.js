07d0ba10fb24932666326da88f4ebf83fa226144
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-06-23 13:50:14 -0400

    saturday --> Shabbos

--- src/main/resources/static/assets/js/custom.js
+++ src/main/resources/static/assets/js/custom.js
@@ -156,3 +156,28 @@ jQuery(function($) {
 //  });
 
 });
+
+// Shabbos 
+function replaceSaturdayWithShabbat() {
+  // Get the root node of the document
+  const root = document.documentElement;
+
+  // Create a tree walker to get all text nodes
+  const treeWalker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null, false);
+
+  // Loop through all text nodes and replace the text
+  let node;
+  while (node = treeWalker.nextNode()) {
+    const text = node.textContent;
+
+    // Replace 'Saturday' with 'Shabbat' in the text content
+    const replacedText = text.replace(/\bSaturday\b/g, 'Shabbat');
+
+    // If the text content was modified, update the node
+    if (text !== replacedText) {
+      node.textContent = replacedText;
+    }
+  }
+}
+
+replaceSaturdayWithShabbat();
