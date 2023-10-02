f99079cfb33d101c0712b29e391492740619f71b
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-10-02 15:38:47 -0400

    make settings avail on all admin pages

--- src/main/java/com/tbdev/teaneckminyanim/admin/controllers/AdminController.java
+++ src/main/java/com/tbdev/teaneckminyanim/admin/controllers/AdminController.java
@@ -12,7 +12,6 @@ import com.tbdev.teaneckminyanim.admin.structure.user.TNMUser;
 import com.tbdev.teaneckminyanim.admin.structure.user.TNMUserDAO;
 import com.tbdev.teaneckminyanim.global.Nusach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties.Settings;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -176,6 +175,14 @@ public class AdminController {
         mv.getModel().put("success", successMessage);
         mv.getModel().put("error", errorMessage);
         return mv;
+    }
+
+    @ModelAttribute("settings")
+    public List<TNMSettings> settings() {
+        // Load and return the settings here
+        List<TNMSettings> settings = this.tnmsettingsDAO.getAll();
+        Collections.sort(settings, Comparator.comparing(TNMSettings::getId)); // sort by id
+        return settings;
     }
 
     @GetMapping("/admin/settings")
