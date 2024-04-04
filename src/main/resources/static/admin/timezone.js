81ab76afff4d347ff1c603abba0f45dd40e839d1
Jacob Rosenfeld <me@jacobrosenfeld.me>
2024-04-04 14:08:40 -0400

    passing variable still not working yet

--- src/main/resources/static/admin/timezone.js
+++ src/main/resources/static/admin/timezone.js
@@ -48,7 +48,7 @@ function populateTimezones() {
         // Listen for change event and update the corresponding input field
         selectElement.on('change', function() {
             const selectedValue = $(this).val();
-            const inputField = $(this).siblings('input[type="timezone"]');
+            const inputField = $('input[type="timezone"]');
             inputField.val(selectedValue).trigger('change'); // Trigger change event for input field if needed
         });
     });
