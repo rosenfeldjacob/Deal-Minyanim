3e52c0d115fdcee598b768dc55201c52edbd93b2
Jacob Rosenfeld <me@jacobrosenfeld.me>
2024-05-28 00:32:08 -0400

    updates edot-home

--- src/main/resources/static/edot-home.js
+++ src/main/resources/static/edot-home.js
@@ -5,10 +5,14 @@ function replacePrayerNames() {
   
     // Loop through each row
     for (var i = 0; i < rows.length; i++) {
-      // Check if 'Edot Hamizrach' is in the row
-      if (rows[i].getElementsByClassName('p-notes')[0].innerHTML.includes('Edot Hamizrach')) {
-        // Replace 'Shacharis' with 'Shacharit' and 'Maariv' with 'Arvit' in that row
-        rows[i].innerHTML = rows[i].innerHTML.replace(/Shacharis/g, 'Shacharit').replace(/Maariv/g, 'Arvit');
+      // Check if the row has an element with class 'p-notes'
+      var pNotes = rows[i].getElementsByClassName('p-notes');
+      if (pNotes.length > 0) {
+        // Check if 'Edot Hamizrach' is in the row
+        if (pNotes[0].innerHTML.includes('Edot Hamizrach')) {
+          // Replace 'Shacharis' with 'Shacharit' and 'Maariv' with 'Arvit' in that row
+          rows[i].innerHTML = rows[i].innerHTML.replace(/Shacharis/g, 'Shacharit').replace(/Maariv/g, 'Arvit');
+        }
       }
     }
   }
