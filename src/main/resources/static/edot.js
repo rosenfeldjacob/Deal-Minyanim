3b344a149f96d29c5b4066d7bf4560321c6bc0d4
Jacob Rosenfeld <me@jacobrosenfeld.me>
2024-05-28 00:44:36 -0400

    button extnesion

--- src/main/resources/static/edot.js
+++ src/main/resources/static/edot.js
@@ -15,8 +15,22 @@ function replacePrayerNames() {
         }
       }
     }
-  }
+
+    // Get the element with 'org-nusach' attribute
+    var orgNusachElement = document.querySelector('[org-nusach]');
+
+    // Check if 'Edot Hamizrach' is the value of 'org-nusach' attribute
+    if (orgNusachElement && orgNusachElement.getAttribute('org-nusach') === 'Edot Hamizrach') {
+      // Get all the button elements on the page
+      var buttons = document.getElementsByTagName('button');
+
+      // Loop through each button
+      for (var i = 0; i < buttons.length; i++) {
+        // Replace 'Shacharis' with 'Shacharit' and 'Maariv' with 'Arvit' in that button
+        buttons[i].innerHTML = buttons[i].innerHTML.replace(/Shacharis/g, 'Shacharit').replace(/Maariv/g, 'Arvit');
+      }
+    }
+}
   
-  // Call the function to perform the replacement
-  replacePrayerNames();
-  
+// Call the function to perform the replacement
+replacePrayerNames();
